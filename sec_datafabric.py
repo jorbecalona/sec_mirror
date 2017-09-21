db7c60773d1563d04403e226fd929c486b75571e
Jonathan Poczatek <chien.jon@gmail.com>
2017-09-21 02:41:47 -0400

    fixed it up

--- sec_datafabric.py
+++ sec_datafabric.py
@@ -12,9 +12,16 @@ from bs4 import BeautifulSoup
 
 import datafabric_settings as settings
 import xmltodict
-
+import json
+import collections
+# from lxml.html import Element, fromstring
+# from xmljson import parker, Parker
+from json import dumps
+import xmltodict
 
 # Test if file exists
+
+
 def getFeedUrl(cik):
     return "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={0}&type=10-K&dateb=&owner=exclude&start=0&count=100&output=atom".format(cik)
 
@@ -23,12 +30,12 @@ def getDefaultFeedUrl(cik):
     return "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={0}&type=&dateb=&owner=exclude&start=0&count=100&output=atom".format(cik)
 
 
-df = pd.read_csv(settings.CIK_LIST_CSV)
-store = pd.HDFStore(settings.LOCAL_HDF)
+# df = pd.read_csv(settings.CIK_LIST_CSV)
+# store = pd.HDFStore(settings.LOCAL_HDF)
 
 # lines = []
 with open(settings.CIK_LIST_PRASANNA, 'r', encoding='latin-1') as f:
-    lines = [x.splitx.split(':')[1] for x in f.readlines()]
+    lines = [x.split(':')[1] for x in f.readlines()]
 
 
 lines = list(set(lines))
@@ -40,13 +47,80 @@ cik_xml = [{'CIK': cik, 'FEED': getDefaultFeedUrl(cik)} for cik in lines]
 # store.put('cik_xml', cik_xml)
 
 
+namespaces = {
+    "http://www.w3.org/2005/Atom": None
+}
+# xmldict = xmltodict.parse(xml, process_namespaces=True, namespaces=namespaces)
+# print(json.dumps(xmldict, indent=4))
+
+
+def parse_xml(feed_url, parsed_dict=None):
+    """
+    Download
+    """
+    resp = requests.get(feed_url)
+    raw_dict = xmltodict.parse(
+        resp.content, encoding="ISO-8859-1", process_namespaces=True, namespaces=namespaces)
+
+    def flatten_links(xmldict):
+        links = {}
+        for x in xmldict['feed']['link']:
+            links[x['@rel']] = {
+                'type': x['@type'],
+                'href': x['@href']
+            }
+        return links
+
+    def get_next(xmldict):
+        return [x['@href'] for x in xmldict['feed']['link'] if x['@rel'] == 'next']
+
+    def get_filings(xmldict):
+        return [x for x in xmldict['feed']['entry']]
+
+    if get_next(raw_dict):
+        links = []
+    links.append(feed_url)
+    resp = requests.get(feed_url)
+    soup = BeautifulSoup(resp.content, 'xml')
+
+    # if x['@rel'] == 'next':
+
+    entries = soup.find_all('entry')
+    page_next = soup.select('link[rel="next"]')
+    entries = soup.find_all('entry')
+    # print('Added [{}] entries'.format(len(entries)))
+
+    if page_next:
+        # print('Page_Next')
+        return parse_xml()(page_next[0].get('href'), links)
+    else:
+        return links
+
+
+def listings(CIK):
+    feed_url = getDefaultFeedUrl(CIK)
+
+    resp = requests.get(feed_url)
+    raw_dict = xmltodict.parse(
+        resp.content, encoding="ISO-8859-1", process_namespaces=True, namespaces=namespaces)
+    links = flatten_links(raw_dict)
+
+    def flatten_links(xmldict):
+        links = {}
+        for x in xmldict['feed']['link']:
+            links[x['@rel']] = {
+                'type': x['@type'],
+                'href': x['@href']
+            }
+        return links
+
+
 def parse_feed(feed_url, links=None):
     if not links:
         links = []
     links.append(feed_url)
     resp = requests.get(feed_url)
     soup = BeautifulSoup(resp.content, 'xml')
-
     entries = soup.find_all('entry')
     page_next = soup.select('link[rel="next"]')
     entries = soup.find_all('entry')
@@ -81,60 +155,15 @@ test = xmltodict.parse(requests.get(testfeeds[0]).content)
 len(test['feed']['entry'])
 
 
+# from xml.etree.ElementTree import fromstring
+def flatten_links(xmldict):
+    links = {}
+    for x in xmldict['feed']['link']:
+        links[x['@rel']] = {
+            'type': x['@type'],
+            'href': x['@href']
+        }
+    return links
+
 # 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001260507&type=&datea=&dateb=&owner=exclude&count=100&output=atom&start=100'
-
-
-# dict_keys(['@xmlns', 'author', 'company-info',
-#    'entry', 'id', 'link', 'title', 'updated'])
-
-# Get author
-
-# Get company-info
-# addresses
-# address
-# type
-# city
-# state
-# street
-# zip
-
-
-# Get entry
-
-# Get id
-
-# Get link
-
-# Get title
-
-# Get updated
-
-
-# unique_ciks = pd.Series.from_array(lines)
-
-# Test if exists
-
-# store.put('companies', df)
-
-# s2 = s.map(lambda x: 'this is a string {}'.format(x),
-#                na_action=None)
-
-# "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={0}&type=10-K&dateb=&owner=exclude&start=0&count=100&output=atom"
-
-
-# Handle pagenation
-
-# mapped = pd.DataFrame({'CIK': (pd.Series(df['CIK'].unique())),
-#         'XMLURL': (pd.Series(df['CIK'].unique())).map(lambda x: getDefaultFeedUrl(x), na_action=None)
-# })
-
-# mapped = pd.DataFrame({'CIK': unique_ciks,
-#         'XMLURL': unique_ciks.map(lambda x: getDefaultFeedUrl(x), na_action=None)
-# })
-
-# store.put('mapped', mapped)
-
-
-# r = requests.get()
-
-# grab all entries
+# xmldict['keys'] ['author', 'company-info', 'entry', 'id', 'link', 'title', 'updated']
